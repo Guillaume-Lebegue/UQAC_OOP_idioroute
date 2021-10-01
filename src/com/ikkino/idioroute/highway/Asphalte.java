4225086cf52ee809bee7aac63098b4ee38b4c7dd
OualidBougzime <55579475+OualidBougzime@users.noreply.github.com>
2021-10-01 00:59:03 -0400

    public coefficient

--- src/com/ikkino/idioroute/highway/Asphalte.java
+++ src/com/ikkino/idioroute/highway/Asphalte.java
@@ -2,7 +2,7 @@ package com.ikkino.idioroute.highway;
 
 public class Asphalte implements Material{
 
-    private final float coefficient = 1.4;
+    public final float coefficient = 1.4;
 
     public float getSpeed(float speed){
         return speed * coefficient;

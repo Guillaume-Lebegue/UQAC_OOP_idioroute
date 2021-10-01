0d680ce2d7661145a445bef10976ae246f826b92
OualidBougzime <55579475+OualidBougzime@users.noreply.github.com>
2021-10-01 01:00:02 -0400

    public coefficient

--- src/com/ikkino/idioroute/highway/Pave.java
+++ src/com/ikkino/idioroute/highway/Pave.java
@@ -2,7 +2,7 @@ ckage com.ikkino.idioroute.highway;
 
 public class Pave implements Material{
 
-    private final float coefficient = 0.6;
+    public final float coefficient = 0.6;
 
     public float getSpeed(float speed){
         return speed * coefficient;

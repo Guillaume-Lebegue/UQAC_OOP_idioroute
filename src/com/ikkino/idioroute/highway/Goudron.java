f82aac9315d0d92f862baf30a57d86abd2903cea
OualidBougzime <55579475+OualidBougzime@users.noreply.github.com>
2021-10-01 00:59:43 -0400

    public coefficient

--- src/com/ikkino/idioroute/highway/Goudron.java
+++ src/com/ikkino/idioroute/highway/Goudron.java
@@ -2,7 +2,7 @@ ckage com.ikkino.idioroute.highway;
 
 public class Goudron implements Material{
 
-    private final float coefficient = 1;
+    public final float coefficient = 1;
 
     public float getSpeed(float speed){
         return speed * coefficient;

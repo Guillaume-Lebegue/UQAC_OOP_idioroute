fa4092bcc32b0aae516a876b099d577ebece05fb
Ikkino <guillaume.lebegue@epitech.eu>
2021-10-10 23:33:41 -0400

    [UP] Removed Hello World

--- src/com/ikkino/idioroute/Idioroute.java
+++ src/com/ikkino/idioroute/Idioroute.java
@@ -4,7 +4,6 @@ import com.ikkino.idioroute.highway.HighwayManager;
 
 final public class Idioroute {
     public static void main(String[] args) {
-        System.out.println("Hello world !");
         HighwayManager highwayManager = new HighwayManager();
         highwayManager.start();
     }
